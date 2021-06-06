d10dfc3635e13d30f238cb360fded00ca8f3c305
Thatch186 <a93275@alunos.uminho.pt>
2021-06-06 19:33:30 +0100

    Comunicação both ways

--- src/aurras.c
+++ src/aurras.c
@@ -12,25 +12,38 @@
 
 int  main(int argc, char *agrv[]){
 
-    //mkfifo("tmp/FifoC", 0666);
 
-    printf("In cliente\n");
-    char buff[MAX_BUFF_SIZE];
-    int bytes_read, fd;
+    printf("[CLIENTE]\n");
+    char buff_wr[MAX_BUFF_SIZE];
+    char buff_rd[MAX_BUFF_SIZE];
+    int bytes_input , bytes_server, fd_fifo_s, fd_fifo_c;
 
-    if((fd = open("tmp/FifoS",O_WRONLY)) == -1){ //pipe ou escrita ou leitura
+    if((fd_fifo_s = open("tmp/FifoS",O_WRONLY)) == -1){ //pipe escrita
         perror("open");
     }else{
-        printf("opened fifo for writing");
+        printf("opened fifoS for writing");
     }
 
-    while( (bytes_read = read(0 ,buff ,MAX_BUFF_SIZE)) > 0){
-        if(write(fd,buff,bytes_read) == -1){
-            perror("write");
+    if((fd_fifo_c = open("tmp/FifoC",O_RDONLY)) == -1){ //pipe leitura
+        perror("open");
+    }else{
+        printf("opened fifoC for reading");
+    }
+
+    while( (bytes_input = read(0 ,buff_wr ,MAX_BUFF_SIZE)) > 0){  //escreve para o servidor, mas se a primeira letra inserida for c, recebe uma mensagem
+        if(write(fd_fifo_s,buff_wr,bytes_input) == -1){
+            perror("write in");
         }
+        if(buff_wr[0] == 'c'){ 
+            bytes_server = read( fd_fifo_c ,buff_rd ,MAX_BUFF_SIZE);
+            if(write(1,buff_rd,bytes_server) == -1)
+                perror("write of");
+        }
+        
     }
 
-    close(fd);
+    close(fd_fifo_s);
+    close(fd_fifo_c);
 
     return 0; 
 }
--- src/aurrasd.c
+++ src/aurrasd.c
@@ -12,21 +12,41 @@
 int main(int argc, char *argv[]){
 
     mkfifo("tmp/FifoS", 0666);
+    mkfifo("tmp/FifoC", 0666);
 
-    printf("In server\n");
-    int fd, bytes_read;
-    char buff[MAX_BUFF_SIZE];
+    printf("[SERVER]\n");
+    int fd_fifo_s , fd_fifo_c; 
+    int bytes_read , bytes_input;
+    char buff_read[MAX_BUFF_SIZE];
+    char buff_write[MAX_BUFF_SIZE];
 
-    if((fd=open("tmp/FifoS", O_RDONLY)) == -1){
-        perror("open");
+    if((fd_fifo_s=open("tmp/FifoS", O_RDONLY)) == -1){
+        perror("open1");
     }
     else{
         printf("Opened fifo for read only");
     }
-    while( (bytes_read = read(fd ,buff ,MAX_BUFF_SIZE)) > 0){
-        if(write(1,buff,bytes_read) == -1){
+
+    if((fd_fifo_c=open("tmp/FifoC", O_WRONLY)) == -1){
+        perror("open2");
+    }
+    else{
+        printf("Opened fifoC for writing only");
+    }
+
+    while( (bytes_read = read(fd_fifo_s ,buff_read ,MAX_BUFF_SIZE)) > 0){ //recebe do cliente, mas se a primeira letra recebida for 'c' manda uma mensagem
+        if(write(1,buff_read,bytes_read) == -1){
             perror("write");
         }
+        if(buff_read[0] == 'c'){ 
+            bytes_input = read(0,buff_write ,MAX_BUFF_SIZE);
+            if(write(fd_fifo_c,buff_write,bytes_input) == -1){
+                perror("write");
+            }
+            //break;
+        }
+        bytes_read=0;
+        bytes_input=0;
     }
     return 0;
 }
